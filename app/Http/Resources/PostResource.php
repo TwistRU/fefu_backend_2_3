68c350da74ef33932c6993e5074545edcc7a2835
TwistRU <roman.pavlyuk.02@mail.ru>
2022-01-11 18:45:54 +1000

    Lesson 9: Add posts and comments with API

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array|Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'text' => $this->text,
+            'created_at' => $this->created_at->format('d-m-Y H:i'),
+            'updated_at' => $this->updated_at->format('d-m-Y H:i'),
+            'author' => new UserResource($this->user),
+            'comments' => CommentResource::collection($this->comments)
+        ];
+    }
+}
